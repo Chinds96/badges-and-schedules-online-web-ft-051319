02a2151df5031da1aefa26cb971c0b2a6379b2ab
Christopher Hinds <christopherhinds96@gmail.com>
2019-05-21 17:54:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+
+
+def badge_maker(name) 
+  return "Hello, my name is #{name}."
+end 
+  
+  
+def batch_badge_creator(attendees)
+ attendees.map{|name| badge_maker(name)}
+end 
+
+def assign_rooms(attendees)
+  attendees.map.with_index(1) do |name, room|
+    puts "Hello, #{name}! You'll be assigned to room #{room}!"
+end 
